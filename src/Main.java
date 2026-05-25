72ca2c711a320b8fccfd0de333bfeef695f1591e
ndri eunice <eunicenini3@gmail.com>
2026-05-25 16:21:25 +0000

    Creation de la boucle de devinette

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // nombre aléatoire entre 1 et 100
         int randomNumber = (int)(Math.random() * 100) + 1;
-    //
+       for(int i = 0; i <=10; i++){
+
+       }
 
     }
 
