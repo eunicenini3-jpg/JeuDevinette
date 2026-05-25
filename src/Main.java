f110a595c7cefabd1155961bb443762579d78b62
ndri eunice <eunicenini3@gmail.com>
2026-05-25 16:13:19 +0000

    Nombre éléatoire généré

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // nombre aléatoire entre 1 et 100
+        int randomNumber = (int)(Math.random() * 100) + 1;
+    //
+
+    }
+
+
+}
