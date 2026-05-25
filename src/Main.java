0186dc8c9dd9cc1dbabf1cd48aa56eaaf77f9035
ndri eunice <eunicenini3@gmail.com>
2026-05-25 16:35:22 +0000

    Lecture en entrée du nombre

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // nombre aléatoire entre 1 et 100
         int randomNumber = (int)(Math.random() * 100) + 1;
        for(int i = 0; i <=10; i++){
-
+           Scanner scanner = new Scanner(System.in);
+           System.out.print("Saisissez un nombre entier svp : ");
+           int nombreUtilisateur = scanner.nextInt();
        }
 
     }
