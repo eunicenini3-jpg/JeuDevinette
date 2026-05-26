29c464fcfc16c3120332fcafa7ec5a3dbf9a4c61
ndri eunice <eunicenini3@gmail.com>
2026-05-26 14:01:20 +0000

    affichage des resultats du jeu

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,28 @@ public class Main {
     public static void main(String[] args) {
         // nombre aléatoire entre 1 et 100
         int randomNumber = (int)(Math.random() * 100) + 1;
-       for(int i = 0; i <=10; i++){
-           Scanner scanner = new Scanner(System.in);
-           System.out.print("Saisissez un nombre entier svp : ");
-           int nombreUtilisateur = scanner.nextInt();
-           if(randomNumber < nombreUtilisateur){
-               System.out.print("Nombre\n" +
-                       "proposé plus grand");
-           } else if (randomNumber > nombreUtilisateur) {
-               System.out.print("Nombre proposé plus\n" +
-                       "petit");
-           } else{
-               System.out.print("Bravoooooo! vous avez gagnez");
-           }
-           if(i == 9){
-               System.out.print("Desoléééééééé, vous avez perdu!");
-           }
-       }
+        Scanner scanner = new Scanner(System.in);
+
+        //Saisie du nombre de l'utilisateur
+        for(int i = 0; i < 10; i++){
+            System.out.print("Saisissez un nombre entier svp : ");
+            int nombreUtilisateur = scanner.nextInt();
+
+            // comparaison et affichage des résultats
+            if(randomNumber == nombreUtilisateur){
+                System.out.println("Bravoooooo! vous avez gagné");
+                break;
+
+            } else if(randomNumber > nombreUtilisateur){
+                System.out.println("Nombre proposé plus petit");
+
+            } else{
+                System.out.println("Nombre proposé plus grand");
+            }
+            if(i == 9){
+                System.out.println("Désolée, vous avez perdu!");
+            }
+        }
+        scanner.close();
     }
 }
